7b9eb9764e2ca6c9c46bda4e28bc0c3d6938e76d
Akshata Chaudhari <55447291+theAkshata07@users.noreply.github.com>
2021-06-20 11:43:51 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,128 @@
+
+
+#e = 0
+#m = 0
+import nltk
+#nltk.download('popular')
+#nltk.download('wordnet')
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+import pickle
+import numpy as np
+
+from keras.models import load_model
+model = load_model('model.h5')
+import json
+import random
+
+from googletrans import Translator
+translator = Translator()
+
+data_file = open('data.json').read()
+intents = json.loads(data_file,strict=False)
+#intents = json.loads(open('data.json').read())
+words = pickle.load(open('texts.pkl','rb'))
+classes = pickle.load(open('labels.pkl','rb'))
+
+
+def clean_up_sentence(sentence):
+   lang=translator.detect(sentence).lang
+   if lang == 'en':
+ 
+       sentence_words = nltk.word_tokenize(sentence)
+        # stem each word - create short form for word
+       sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+       return sentence_words
+   elif lang == 'mr':
+      
+       a = translator.translate(sentence, dest='en')
+
+    # tokenize the pattern - split words into array
+    #if sentence=="1":
+       sentence_words1 = nltk.word_tokenize(a.text)
+        # stem each word - create short form for word
+       sentence_words1 = [lemmatizer.lemmatize(word.lower()) for word in sentence_words1]
+       return sentence_words1
+   elif lang == 'hi':
+      
+       a = translator.translate(sentence, dest='en')
+
+    # tokenize the pattern - split words into array
+    #if sentence=="1":
+       sentence_words2 = nltk.word_tokenize(a.text)
+        # stem each word - create short form for word
+       sentence_words2 = [lemmatizer.lemmatize(word.lower()) for word in sentence_words2]
+       return sentence_words2
+
+# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+
+def bow(sentence, words, show_details=True):
+    # tokenize the pattern
+    sentence_words = clean_up_sentence(sentence)
+    # bag of words - matrix of N words, vocabulary matrix
+    bag = [0]*len(words)  
+    for s in sentence_words:
+        for i,w in enumerate(words):
+            if w == s: 
+                # assign 1 if current word is in the vocabulary position
+                bag[i] = 1
+                if show_details:
+                    print ("found in bag: %s" % w)
+    return(np.array(bag))
+
+def predict_class(sentence, model):
+    # filter out predictions below a threshold
+    p = bow(sentence, words,show_details=False)
+    res = model.predict(np.array([p]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
+    # sort by strength of probability
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    return return_list
+
+def getResponse(ints, intents_json):
+    tag = ints[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if(i['tag']== tag):
+            result = random.choice(i['responses'])
+            break
+    return result
+
+def chatbot_response(msg):
+    ints = predict_class(msg, model)
+    res = getResponse(ints, intents)
+    lang = translator.detect(msg).lang
+    if lang == 'en':
+        return res
+    elif lang == 'mr':
+        b = translator.translate(res, dest='mr')
+        c=b.text
+        return c
+    elif lang == 'hi':
+        d = translator.translate(res, dest='hi')
+        e=d.text
+        return e
+
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+app.static_folder = 'static'
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    #sen = request.args.get('msg-info')
+    return chatbot_response(userText)
+
+
+if __name__ == "__main__":
+    app.run()
